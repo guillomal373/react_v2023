d723608839b275755925a8cf26b2c85d29ccdd73
Guillermo Malagon <guillermo@mrvaldemar.com>
2023-10-24 16:31:59 -0500

    creacion y const let

---
+++ fernando-herrera/02-intro-javascript/bases/01-const-let.js
@@ -0,0 +1,15 @@
+//variables y constantes
+const nombre = 'guillo';//CONST variables q no se van a renombrar
+const apellido = 'malagon';//si va a cambiar, sino va a cambiar LET
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if ( true ){
+    let valorDado = 6;
+    console.log( valorDado );
+}
+
+console.log( valorDado );
