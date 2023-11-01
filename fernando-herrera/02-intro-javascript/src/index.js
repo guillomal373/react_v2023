5d54b08c1473703df8e1ce9960dcfb079685731b
Guillermo Malagon <guillermo@mrvaldemar.com>
2023-10-31 21:51:56 -0500

    simple promise without homework

--- fernando-herrera/02-intro-javascript/src/index.js
+++ fernando-herrera/02-intro-javascript/src/index.js
@@ -1,1 +1,14 @@
-//
+//promises 
+//https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Promise
+
+//resolve cuando se cumpla la promesa y reject cuando pasa algo y no se respondió positivamente
+const promise = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+        resolve();//console.log('2 seconds later')
+    }, 2000);
+});
+
+promise
+    .then( ()=>{ console.log('Then of the promise 2 seconds later'); } )//when promise ends
+    .catch( ()=> {} )
+    .finally( ()=>{} )
