9feabebbe89a0fc1aaff8642010c22c7582810b4
Guillermo Malagon <guillermo@mrvaldemar.com>
2023-11-05 20:05:39 -0500

    async

--- fernando-herrera/02-intro-javascript/src/index.js
+++ fernando-herrera/02-intro-javascript/src/index.js
@@ -1,24 +1,14 @@
-//fetch API -> https://developer.mozilla.org/es/docs/Web/API/Fetch_API
-const apiKey='MQTeHY7O1dmlmUnG0886vdMxNcaFPSMJ'; //Giphy -> https://developers.giphy.com/
+//Async | Await
 
-//RANDOM
-const peticionGiphiApi = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+// sin Async | Await
+//const getImagenPromesa = () => new Promise((resolve)=>{resolve('http://www.mrvaldemar.com/asdf')})
+//getImagenPromesa().then(console.log); 
 
-//SEARCHING A WORD
-//const searchWord = 'excellent';
-//const peticionGiphiApi = fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchWord}`);
 
-peticionGiphiApi
-    .then( result=>result.json() )
-        .then( ({data}) =>{//promesa anidada, se desestructura pq vienen la llave "data" dentro de "data" es decir {data}
-                console.log(data);
-                //RANDOM returns only one result
-                const {url} = data.images.original;
+//con Async | Await
+const getImagen = async() => {
+    return 'http://www.mrvaldemar.com/asdf';
+    //el await ayuda a q el código sea SÍNCRONO
+}
 
-                //SEARCHING A WORD RETURNS AN ARRAY
-                //const {url} = data[0].images.original;
-                const img = document.createElement('img');
-                img.src = url;
-                document.body.append(img);
-        } )
-    .catch( () => { console.warn })  
+getImagen().then(console.log);
