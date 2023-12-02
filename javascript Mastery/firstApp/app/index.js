7d0232fdd591232ec02fcd4c969cc5c4e45e5315
Guillermo Malagon <guillermo@mrvaldemar.com>
2023-12-01 19:45:12 -0500

    ScreenHeaderButton

--- javascript Mastery/firstApp/app/index.js
+++ javascript Mastery/firstApp/app/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { View, ScrollView, SafeAreaView, Text, Button } from "react-native";
 import { Stack, useRouter } from 'expo-router';
 import { COLORS, icons, images, SIZES } from '../constants';
 import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
@@ -8,24 +8,31 @@ const Home = () => {
     const router = useRouter();
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
-            <Stack.Screen options={{
-                headerStyle: {backgroundColor: COLORS.lightWhite},
-                headerShadowVisible: false,
-                headerLeft: () => {
-                    <ScreenHeaderBtn iconUrl = {icons.menu} dimension = "60%"></ScreenHeaderBtn>
-                },
-                headerRight: () => {
-                    <ScreenHeaderBtn iconUrl = {icons.profile} dimension = "100%"></ScreenHeaderBtn>
-                },
-                headerTitle: " index "
-            }}/>
+            {/*OPCIÓN 1*/}
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <ScreenHeaderBtn iconUrl = {icons.menu} dimension = "60%"/>
+                    <ScreenHeaderBtn iconUrl = {images.profile} dimension = "60%"/>
+            </View>
+            {/*OPCIÓN 2*/}
+            <Stack.Screen 
+                options={{
+                    headerStyle: {backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: true,
+                    headerLeft: () => {
+                        <ScreenHeaderBtn iconUrl = {icons.menu} dimension = "60%"></ScreenHeaderBtn>
+                    },
+                    headerRight: () => {
+                        <ScreenHeaderBtn iconUrl = {icons.profile} dimension = "100%"></ScreenHeaderBtn>
+                    },
+                    headerTitle: "  "
+                }}
+            />
 
             <ScrollView showsVerticalScrollIndicator={false}>
                 <View style={{flex:1, padding: SIZES.medium}}>
                     <Welcome/>
                     <Popularjobs />
                     <Nearbyjobs />
-                    <ScreenHeaderBtn/>
                 </View>
             </ScrollView>
         </SafeAreaView>
