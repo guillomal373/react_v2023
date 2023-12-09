e69d24e00e0e62ece07795eea889778d31c4af18
Guillermo Malagon <guillermo@mrvaldemar.com>
2023-12-09 17:20:50 -0500

    adding try catch

--- fernando-herrera/02-intro-javascript/src/bases/11-async-await.js
+++ fernando-herrera/02-intro-javascript/src/bases/11-async-await.js
@@ -7,14 +7,21 @@
 
 //con Async | Await
 const getImagen = async() => {
-        const apiKey='MQTeHY7O1dmlmUnG0886vdMxNcaFPSMJ'; //Giphy -> https://developers.giphy.com/
-        //como es un response por poner el await antes del fetch entonces va a parar hasta que llegue respuesta pq ahora es RESPONSE
-        const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
-        const {data} = await resp.json();
-        const {url} = data.images.original;
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append(img);
+
+        try {
+                //como es un response por poner el await antes del fetch entonces va a parar hasta que llegue respuesta pq ahora es RESPONSE
+                const apiKey='MQTeHY7O1dmlmUnG0886vdMxNcaFPSMJ'; //Giphy -> https://developers.giphy.com/
+                const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+                const {data} = await resp.json();
+                const {url} = data.images.original;
+                const img = document.createElement('img');
+                img.src = url;
+                document.body.append(img);
+        } catch (error) {
+                console.error('UPS')
+                console.error(error)
+                
+        }
 }
 
 
