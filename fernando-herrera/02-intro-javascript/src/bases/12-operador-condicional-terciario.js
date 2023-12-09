31499a14f42c41e64a6078c45b76cfe904cc8560
Guillermo Malagon <guillermo@mrvaldemar.com>
2023-12-09 17:28:51 -0500

    operador condicional terciario

---
+++ fernando-herrera/02-intro-javascript/src/bases/12-operador-condicional-terciario.js
@@ -0,0 +1,10 @@
+//Operador condicional ternario
+
+const activo = true;
+
+const mensaje =  ( activo ) ? 'Activo' : 'Inactivo';
+console.log(mensaje);
+
+//SOLO VERIFICAR SI ES VERDAD SINO RETORNAR FALSO
+const mensaje2 = activo && 'Activo';
+console.log(mensaje2);
