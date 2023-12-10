742f6f3e3b6c6a16802459c3017435694413c98d
Ing Guillermo Malagon <guillomal373@gmail.com>
2023-12-09 20:22:27 -0500

    examples for render data from variables

--- fernando-herrera/03-counter-app-vite/src/HelloApp.jsx
+++ fernando-herrera/03-counter-app-vite/src/HelloApp.jsx
@@ -1,5 +1,27 @@
-function App(){
-    return (<h1>Hola Guillo!!</h1>);
-}
+const nombre = 'Guillo';
+const newMessage = [1,2,3,4,5,6,7,8,9];
+const persona = {id:1, name: 'Guillermo', age: 39, wear: {top: 'white', bottom: 'blue'}};
 
-export default App;
+const getMyAge = () => {
+    return persona.age;
+}
+export const App = () => {
+    return (
+        <>
+        <table>
+            <td>
+                <tr>String:</tr>
+                <tr>Array:</tr>
+                <tr>JSONStringify:</tr>
+                <tr>Function: </tr>
+            </td>
+            <td>
+                <tr>{nombre}</tr>
+                <tr>{newMessage}</tr>
+                <tr>{JSON.stringify(persona)}</tr>
+                <tr>{getMyAge()}</tr>
+            </td>
+        </table>
+        </>
+    );
+}
--- fernando-herrera/03-counter-app-vite/src/main.jsx
+++ fernando-herrera/03-counter-app-vite/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import HelloWorldApp from './HelloApp'
+import { App } from './HelloApp'
 
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <HelloWorldApp/>
+        <App/>
     </React.StrictMode>
 );
