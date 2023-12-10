9cb24b7970569dab89fd01ebe2779c53bb035576
Ing Guillermo Malagon <guillomal373@gmail.com>
2023-12-09 19:28:38 -0500

    exportando toda la app

---
+++ fernando-herrera/03-counter-app-vite/src/HelloApp.jsx
@@ -0,0 +1,5 @@
+function App(){
+    return (<h1>Hola Guillo!!</h1>);
+}
+
+export default App;
--- fernando-herrera/03-counter-app-vite/src/main.jsx
+++ fernando-herrera/03-counter-app-vite/src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function App(){
-    return (<h1>Hola Guillo</h1>);
-}
+import HelloWorldApp from './HelloApp'
+
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <App/>
+        <HelloWorldApp/>
     </React.StrictMode>
 );
