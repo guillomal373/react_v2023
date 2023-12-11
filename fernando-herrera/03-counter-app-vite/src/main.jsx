8110eed29b75220a63b7346e6f3470e83cc191b3
Ing Guillermo Malagon <guillomal373@gmail.com>
2023-12-10 23:24:02 -0500

    styles

--- fernando-herrera/03-counter-app-vite/src/main.jsx
+++ fernando-herrera/03-counter-app-vite/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './HelloApp'
+import { App } from './HelloApp';
+
+import './styles.css';
 
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
