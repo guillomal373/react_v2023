540596bcaaedd65549b8ec26b8630c08fe7721e8
Ing Guillermo Malagon <guillomal373@gmail.com>
2023-12-10 23:17:32 -0500

    different type of rendering

--- fernando-herrera/03-counter-app-vite/src/HelloApp.jsx
+++ fernando-herrera/03-counter-app-vite/src/HelloApp.jsx
@@ -1,27 +1,45 @@
 const nombre = 'Guillo';
-const newMessage = [1,2,3,4,5,6,7,8,9];
+const newArray = [1,2,3,4,5,6,7,8,9];
 const persona = {id:1, name: 'Guillermo', age: 39, wear: {top: 'white', bottom: 'blue'}};
 
 const getMyAge = () => {
     return persona.age;
 }
+
 export const App = () => {
     return (
         <>
         <table>
-            <td>
-                <tr>String:</tr>
-                <tr>Array:</tr>
-                <tr>JSONStringify:</tr>
-                <tr>Function: </tr>
-            </td>
-            <td>
-                <tr>{nombre}</tr>
-                <tr>{newMessage}</tr>
-                <tr>{JSON.stringify(persona)}</tr>
-                <tr>{getMyAge()}</tr>
-            </td>
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Value</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>String:</td>
+                    <td>{nombre}</td>
+                </tr>
+                <tr>
+                    <td>Array:</td>
+                    <td>{newArray}</td>
+                </tr>
+                <tr>
+                    <td>JSONSstringify:</td>
+                    <td>{JSON.stringify(persona)}</td>
+                </tr>
+                <tr>
+                    <td>Function: </td>
+                    <td>{getMyAge()}</td>
+                </tr>
+            </tbody>
         </table>
+        <br /><br />
+        <div>
+            JSONSstringify CODE:
+            <code>{JSON.stringify(persona)}</code>
+        </div>
         </>
     );
 }
