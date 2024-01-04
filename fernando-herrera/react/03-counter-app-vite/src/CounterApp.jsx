6a3adc4ae5f048947f5142054660425c70061d6a
Ing Guillermo Malagon <guillomal373@gmail.com>
2024-01-03 23:14:50 -0500

    first Hook

--- fernando-herrera/react/03-counter-app-vite/src/CounterApp.jsx
+++ fernando-herrera/react/03-counter-app-vite/src/CounterApp.jsx
@@ -1,18 +1,26 @@
+import { useState } from 'react';//todos los "use..." son hooks
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({
     value,
 }) => {
 
+    //MI primer Hook
+    const [ counter, setCounter ] = useState( value );
+
     //Al estar dentro del FUNCTIONAL COMPONENT, se renderiza en memoria cada vez que se recarga, mejor afuera si no se requiere este renderizado
-    const handleAdd = (event) => {
-        console.log(event)
+    const handleAdd = () => {
+        // console.log(event);
+        //opción 1 para sumar al counter
+        setCounter( counter +1 );
+        //opcion 2 para sumar al counter
+        //setCounter( (c) => c+1 )
     }
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{ value }</h2>
+            <h2>{ value } - { counter }</h2>
 
             {/*EVENTOS  https://es.react.dev/learn/adding-interactivity */}
             <button onClick={handleAdd}>
