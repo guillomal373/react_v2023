581f955bd5284c93d140c7e6351b15d9c1034fe0
Ing Guillermo Malagon <guillomal373@gmail.com>
2024-01-03 23:01:58 -0500

    functions

--- fernando-herrera/react/03-counter-app-vite/src/CounterApp.jsx
+++ fernando-herrera/react/03-counter-app-vite/src/CounterApp.jsx
@@ -1,13 +1,23 @@
 import PropTypes from 'prop-types';
 
-
 export const CounterApp = ({
     value,
 }) => {
+
+    //Al estar dentro del FUNCTIONAL COMPONENT, se renderiza en memoria cada vez que se recarga, mejor afuera si no se requiere este renderizado
+    const handleAdd = (event) => {
+        console.log(event)
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{ value }</h2>
+
+            {/*EVENTOS  https://es.react.dev/learn/adding-interactivity */}
+            <button onClick={handleAdd}>
+                +1
+            </button>
         </>
     )
 };
