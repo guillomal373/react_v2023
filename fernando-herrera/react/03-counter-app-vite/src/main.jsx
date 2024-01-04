0df986c20e144bdd9e10a0210033cb017236651c
Ing Guillermo Malagon <guillomal373@gmail.com>
2024-01-03 22:44:30 -0500

    counterApp first attempt

--- fernando-herrera/react/03-counter-app-vite/src/main.jsx
+++ fernando-herrera/react/03-counter-app-vite/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './HelloApp';
+// import { App } from './HelloApp';
+import { CounterApp } from './CounterApp';
 
 import './styles.css';
 
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <App title='Mi primer App en React' subTitle={123} status={true}/>
+        {/* <App title='Mi primer App en React' subTitle={123} status={true}/> */}
+        <CounterApp value={123}></CounterApp>
     </React.StrictMode>
 );
